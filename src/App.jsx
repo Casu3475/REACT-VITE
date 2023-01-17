7b755689b4503b1e563b53e95d46d35afccbf686
Casu3475 <106447335+Casu3475@users.noreply.github.com>
2023-01-17 12:19:23 +0100

    react+vite+three

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+import { createRoot } from "react-dom/client";
+import React, { useRef, useState, Suspense } from "react";
+import ReactDOM from "react-dom";
+import { OrbitControls } from "@react-three/drei";
+
+function Box(props) {
+  const mesh = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
